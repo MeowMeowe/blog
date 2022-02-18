9bd189c88c9beca2e7b6ef411741ee102f8509cb
“shenmengke” <shenmengke@gaodun.com>
2022-02-18 18:11:56 +0800

    bg

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="home">
       <h1 className="one-piece">静水深流.</h1>
       <p className="text-line">LOVE YOU LOVE YOU MIAOMIAOMIAO·</p>
-      <p className="sample-dec">/*这里是MeowGod的个人主页*/</p>
+      <p className="sample-dec">&gt;&gt;这里是MeowGod的个人主页&lt;&lt;</p>
     </div>
   );
 }
