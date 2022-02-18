1ec637b92aa2546dc24ea29747c433ebf3a2a4e3
“shenmengke” <shenmengke@gaodun.com>
2022-02-18 16:40:43 +0800

    init

--- src/compontents/nav/index.tsx
+++ src/compontents/nav/index.tsx
@@ -6,7 +6,7 @@ export default function Nav () {
     return (
         <nav className="nav-wrap">
             <div className="logo animated bounceIn">
-                <img src="../static/img/cat.png"  alt="logo"/>
+                <img className="logo-img" src="../static/img/cat.png"  alt="logo"/>
                  MeowGod`s Blog 
             </div>
             <Link to="/home">首页</Link>
