aaa6fe96b54fbdcddfbc985fc1213a909165dc86
“shenmengke” <shenmengke@gaodun.com>
2022-02-28 16:08:37 +0800

    loading

--- src/compontents/loading/index.tsx
+++ src/compontents/loading/index.tsx
@@ -5,6 +5,6 @@ import './index.scss';
 export default function Loading() {
     return <div className="loading" style ={{color:randomColor()}}>
       <div className={`loader-${randomNumber(1,29,0)}`} />
-      {/* <div className={`loader-30`} /> */}
+      {/* <div className={`loader-3`} /> */}
     </div>;
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import Loading from "../../compontents/loading";
 import "./index.scss"
 
 export default function Home() {
   return (
-    // <div className="home">
-    //   <h1 className="one-piece">静水深流.</h1>
-    //   <p className="text-line" />
-    //   <p className="sample-dec">&gt;&gt;这里是MeowGod的个人主页&lt;&lt;</p>
-    // </div>
-    <Loading></Loading>
+    <div className="home">
+      <h1 className="one-piece">静水深流.</h1>
+      <p className="text-line" />
+      <p className="sample-dec">&gt;&gt;这里是MeowGod的个人主页&lt;&lt;</p>
+    </div>
   );
 }
