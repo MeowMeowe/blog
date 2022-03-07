889c12e9308052d1f981aa9f59bae3d8bc796e52
“shenmengke” <shenmengke@gaodun.com>
2022-03-07 11:21:57 +0800

    disable devtools on prod

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Loading from "./compontents/loading";
-import React, { Suspense } from "react";
-import { render } from "react-dom";
-import App from "./App";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { disableReactDevTools } from './static/js/utils';
+import Loading from './compontents/loading';
+import React, { Suspense } from 'react';
+import { render } from 'react-dom';
+import App from './App';
+if (process.env.NODE_ENV == 'production') {
+    disableReactDevTools();
+}
 render(
-  <BrowserRouter>
-    {/* 使用了路由懒加载，所以需要使用<Suspense>包起来 */}
-    <Suspense fallback={<Loading />}>
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
-    </Suspense>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <BrowserRouter>
+        {/* 使用了路由懒加载，所以需要使用<Suspense>包起来 */}
+        <Suspense fallback={<Loading />}>
+            <Routes>
+                <Route path="*" element={<App />} />
+            </Routes>
+        </Suspense>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
--- src/static/js/utils.ts
+++ src/static/js/utils.ts
@@ -1,4 +1,4 @@
-export const LoadJs = (url: string, callback?: any) => {
+export const LoadJs = (url: string, callback?: any) :void => {
     let domScript: any = document.createElement('script');
     domScript.src = url;
     let success =
@@ -16,11 +16,11 @@ export const LoadJs = (url: string, callback?: any) => {
     document.getElementsByTagName('head')[0].appendChild(domScript);
 };
 
-export const randomNumber = (min: number, max: number, float: number) => {
+export const randomNumber = (min: number, max: number, float: number) :string => {
     return Math.floor(Math.random() * (max - min + 1) + min).toFixed(float);
 };
 
-export const randomColor = () => {
+export const randomColor = () :string => {
     return (
         'rgb(' +
         ~~(Math.random() * 255) +
@@ -31,4 +31,16 @@ export const randomColor = () => {
         ')'
     );
 };
+
+export const disableReactDevTools = (): void => {
+    const noop = (): void => undefined;
+    const DEV_TOOLS = (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__;
+
+    if (typeof DEV_TOOLS === 'object') {
+        for (const [key, value] of (<any>Object).entries(DEV_TOOLS)) {
+            DEV_TOOLS[key] = typeof value === 'function' ? noop : null;
+        }
+    }
+};
+
 
