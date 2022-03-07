16f3f424d9fb59fe4e360ef1a2d3673062743643
“shenmengke” <shenmengke@gaodun.com>
2022-03-07 18:10:58 +0800

    api complete

--- src/pages/one-word/index.tsx
+++ src/pages/one-word/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { get } from '../../static/js/axios';
 import './index.scss';
 
@@ -6,12 +6,16 @@ export default function Home() {
     const [word, setWord] = useState('');
 
     const getWord = async () => {
-        let res = await get('https://meowgod.com/api/sweet_word/all', {});
+        let res = await get('https://meowgod.com/api/sweet_word/randomOne', {});
         if (res.status == 0 && res.data) {
             setWord(res.data.text);
         }
     };
-    getWord();
+    useEffect(() => {
+        if (!word) {
+            getWord();
+        }
+    }, [word]);
 
     return (
         <div className="one-word">
