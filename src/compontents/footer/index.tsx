bc07f10cac2411f435493b527426298015724691
“shenmengke” <shenmengke@gaodun.com>
2022-03-07 11:32:20 +0800

    ico

--- src/compontents/footer/index.tsx
+++ src/compontents/footer/index.tsx
@@ -1,12 +1,7 @@
-import React, { useEffect } from 'react';
-// import { Link } from 'react-router-dom';
-import { LoadJs } from '../../static/js/utils';
+import React from 'react';
 import './index.scss';
 
 export default function Footer() {
-    useEffect(() => {
-        LoadJs('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js');
-    });
     return (
         <div className="footer" style={{ height: window.innerHeight / 3.5 + 'px' }}>
             <img className="footer-logo" src="./static/img/icon/cat.webp" alt="logo" />
