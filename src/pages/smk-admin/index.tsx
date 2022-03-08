82a36eba335a99d33f9e4ed6f9017720096c2231
“shenmengke” <shenmengke@gaodun.com>
2022-03-08 13:18:04 +0800

    login

--- src/pages/smk-admin/index.tsx
+++ src/pages/smk-admin/index.tsx
@@ -5,5 +5,6 @@ import './index.scss';
 
 export default function SmkAdmin() {
     const [loginStatus, setLoginStatus] = useState(false);
+    setLoginStatus(false)
     return <div className="admin">{loginStatus ? <AdminContainer /> : <Login />}</div>;
 }
